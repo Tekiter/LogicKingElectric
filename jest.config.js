8bb7f9f1cacb24dcacb92ba188fbe61dfa2a82f5
Tekiter <tekiter8@gmail.com>
2021-06-24 22:32:59 +0900

    Create test environment

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    testMatch: ["**/tests/**/*.+(ts|tsx|js)", "<rootDir>/packages/**/?(*.)+(spec|test).+(ts|tsx|js)"],
+    testPathIgnorePatterns: ["/node_modules/", "/test/tmp/", "/test/utils/"],
+    transformIgnorePatterns: [
+        "/node_modules/",
+        "/test/(fixtures|tmp|__data__)/",
+        "<rootDir>/(packages|codemods|eslint)/[^/]+/lib/",
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest",
+    },
+    testEnvironment: "node",
+};
---
+++ packages/server/tests/mytest.ts
@@ -0,0 +1,6 @@
+describe("A simple test set", () => {
+    test("hello test", () => {
+        const x = 3;
+        expect(x + 4).toBe(7);
+    });
+});
