e13bf7900e8257e1989c1cac72414a3d72acf58b
Tekiter <tekiter8@gmail.com>
2021-06-27 23:58:57 +0900

    리팩터링: 컨트롤러 저장 방식 변경

--- packages/server/src/app/api/v1/index.ts
+++ packages/server/src/app/api/v1/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { getControllers, ServiceFacade, Controller } from "./util";
+import { getControllers, ServiceFacade, Controller, asyncErrorHandler } from "./util";
 
 import "./controller";
+import { HTTPMethod } from "@electric/shared/src/api/v1/util";
 
 export class APIv1 {
     private _router = express.Router();
@@ -23,19 +24,30 @@ export class APIv1 {
 
     private setController(controller: Controller) {
         const { method, path } = controller.endpoint;
-        const serviceBindedController = controller(this.services);
+
+        const middleware = (req: express.Request, res: express.Response) => {
+            const request = {
+                data: extractData(method, req),
+            };
+
+            controller
+                .handler(request, this.services)
+                .then(response => res.status(200).json(response))
+                .catch(asyncErrorHandler(res));
+        };
+
         switch (method) {
             case "GET":
-                this._router.get(path, serviceBindedController);
+                this._router.get(path, middleware);
                 break;
             case "POST":
-                this._router.post(path, serviceBindedController);
+                this._router.post(path, middleware);
                 break;
             case "PATCH":
-                this._router.patch(path, serviceBindedController);
+                this._router.patch(path, middleware);
                 break;
             case "DELETE":
-                this._router.delete(path, serviceBindedController);
+                this._router.delete(path, middleware);
                 break;
             default:
                 throw new Error("Invalid endpoint method:" + method);
@@ -50,6 +62,24 @@ export class APIv1 {
         this._router.use(express.json());
         this._router.use(express.urlencoded({ extended: true }));
     }
+}
+
+function extractData<T>(method: HTTPMethod, req: express.Request): T {
+    let dataSource = req.body;
+    switch (method) {
+        case "GET":
+        case "DELETE":
+            dataSource = req.query;
+            break;
+        case "PATCH":
+        case "POST":
+            dataSource = req.body;
+            break;
+        default:
+            throw new Error("Invalid method type : " + method);
+    }
+
+    return dataSource as T;
 }
 
 export default APIv1;
--- packages/server/src/app/api/v1/util/controller.ts
+++ packages/server/src/app/api/v1/util/controller.ts
@@ -1,7 +1,5 @@
-import express from "express";
 import { EndpointEntry } from "@electric/shared/src/api/v1/util";
 import { DataAccess } from "../../../../service/dataAccess/types";
-import { asyncErrorHandler } from "./error";
 
 export interface ServiceFacade {
     dataAccess: DataAccess;
@@ -10,55 +8,29 @@ export interface ServiceFacade {
 export interface Request<Data> {
     data: Data;
 }
-
 export interface RequestHandler<RequestData, ResponseData> {
     (request: Request<RequestData>, services: ServiceFacade): Promise<ResponseData>;
 }
 
 export interface Controller {
-    (services: ServiceFacade): (req: express.Request, res: express.Response) => void;
+    handler: RequestHandler<unknown, unknown>;
     endpoint: EndpointEntry;
+    flags: {
+        needAuth: boolean;
+    };
 }
 
 export function createNoAuthController<Req, Res>(
     endpoint: EndpointEntry,
     handler: RequestHandler<Req, Res>,
 ): Controller {
-    const controller: Controller =
-        services =>
-        (req, res): void => {
-            const request: Request<Req> = {
-                data: extractData(endpoint, req),
-            };
-            console.log(request);
-
-            handler(request, services)
-                .then(response => res.status(200).json(response))
-                .catch(asyncErrorHandler(res));
-        };
-
-    controller.endpoint = endpoint;
-    return controller;
-}
-
-function extractData<T>(endpoint: EndpointEntry, req: express.Request): T {
-    let dataSource = req.body;
-    console.log(req.body);
-    console.log(req.query);
-    switch (endpoint.method) {
-        case "GET":
-        case "DELETE":
-            dataSource = req.query;
-            break;
-        case "PATCH":
-        case "POST":
-            dataSource = req.body;
-            break;
-        default:
-            throw new Error("Invalid method type : " + endpoint.method);
-    }
-
-    return dataSource as T;
+    return {
+        handler: handler as RequestHandler<unknown, unknown>,
+        endpoint: endpoint,
+        flags: {
+            needAuth: false,
+        },
+    };
 }
 
 const controllers: Controller[] = [];
