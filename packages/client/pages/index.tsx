e082bdba01a67963f4d45dfe9a0a2501c0b6c440
goseungduk <a457581@catholic.ac.kr>
2021-07-02 16:02:25 +0900

    210702_프론트_디렉터리_정돈

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import Login from "./components/login";
 
 export default function Home(): JSX.Element {
+    // Ctrl+Space : check children
+    // Don't use arrow function
     return (
         <div>
             <Head>
@@ -8,7 +11,7 @@ export default function Home(): JSX.Element {
                 <meta name="description" content="" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
+            <Login></Login>
             <main></main>
 
             <footer></footer>
