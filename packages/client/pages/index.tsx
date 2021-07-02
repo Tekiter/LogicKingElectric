34118315b3b30d01d40b6cb1b78204127cb669ee
Tekiter <tekiter8@gmail.com>
2021-07-02 23:36:03 +0900

    프로젝트: 모호점 수정

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Login from "./components/login";
 
 export default function Home(): JSX.Element {
     // Ctrl+Space : check children
@@ -12,7 +11,6 @@ export default function Home(): JSX.Element {
                 <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Login></Login>
             <main></main>
 
             <footer></footer>
