7508c92869d9965411a20f6989978b0da7183e9c
Tekiter <tekiter8@gmail.com>
2021-07-05 22:06:03 +0900

    리팩터링: API hook 개선

--- packages/client/pages/testpage/login.tsx
+++ packages/client/pages/testpage/login.tsx
@@ -1,8 +1,8 @@
-import apiService from "@/api";
 import { useAuthToken, useAuthTokenDestroyer, useAuthTokenSetter } from "@/state/auth";
 import { issueToken } from "@/api/endpoint";
 import { Button, Container, TextField } from "@material-ui/core";
 import { useState } from "react";
+import { useAPIRequest } from "@/api/hooks";
 
 export default function LoginTest(): JSX.Element {
     const authToken = useAuthToken();
@@ -16,37 +16,29 @@ export default function LoginTest(): JSX.Element {
 }
 
 function LoginBox() {
-    const [response, setResponse] = useState("");
-
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const setAuthToken = useAuthTokenSetter();
 
-    async function requestAPI() {
-        try {
-            const res = await apiService.request(issueToken.endpoint, {
-                username,
-                password,
-            });
+    const { request, data, error, isLoading } = useAPIRequest(issueToken.endpoint, {
+        onSuccess(res) {
             setAuthToken(res.accessToken);
-        } catch (error) {
-            if (issueToken.authFailError(error)) {
-                setResponse("Auth Failed : " + error.message);
-            }
-        }
-    }
+        },
+    });
 
     return (
         <div>
             <TextField label="username" value={username} onChange={e => setUsername(e.target.value)} />
             <TextField label="password" value={password} onChange={e => setPassword(e.target.value)} />
             <div>
-                <Button color="primary" onClick={requestAPI}>
+                <Button color="primary" onClick={() => request({ username, password })}>
                     Sign In
                 </Button>
             </div>
-            <p>{response}</p>
+            <p>{isLoading ? "Loading..." : ""}</p>
+            <p>{JSON.stringify(data)}</p>
+            <p>{error + ""}</p>
         </div>
     );
 }
--- packages/client/pages/testpage/register.tsx
+++ packages/client/pages/testpage/register.tsx
@@ -1,37 +1,36 @@
-import apiService from "@/api";
 import { register } from "@/api/endpoint";
+import { useAPIRequest } from "@/api/hooks";
 import { Button, Container, TextField } from "@material-ui/core";
 import { useState } from "react";
 
 export default function LoginTest(): JSX.Element {
-    const [response, setResponse] = useState("");
-
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    async function requestAPI() {
-        try {
-            const res = await apiService.request(register.endpoint, {
-                username,
-                password,
-            });
-            setResponse(res.success + "");
-        } catch (error) {
-            if (register.registerFailError(error)) {
-                setResponse("Register Failed : " + error.message);
+    const requestRegister = useAPIRequest(register.endpoint);
+
+    function ShowError() {
+        if (requestRegister.error) {
+            if (register.registerFailError(requestRegister.error)) {
+                return <div>Register Fail</div>;
             }
+            return <div>Fatal Error</div>;
         }
+        return <></>;
     }
+
     return (
         <Container>
             <TextField label="username" value={username} onChange={e => setUsername(e.target.value)} />
             <TextField label="password" value={password} onChange={e => setPassword(e.target.value)} />
             <div>
-                <Button color="primary" onClick={requestAPI}>
+                {requestRegister.isLoading ? "Loading..." : ""}
+                <Button color="primary" onClick={() => requestRegister.request({ username, password })}>
                     Sign up
                 </Button>
             </div>
-            <div>{response}</div>
+            <div>{requestRegister.data}</div>
+            <ShowError />
         </Container>
     );
 }
