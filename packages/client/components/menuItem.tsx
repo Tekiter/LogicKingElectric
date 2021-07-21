3dd4683c2979c48e9bcd090178d99187046c43ea
goseungduk <a457581@catholic.ac.kr>
2021-07-21 19:24:08 +0900

    210721_메뉴디자인수정

--- packages/client/components/menuItem.tsx
+++ packages/client/components/menuItem.tsx
@@ -1,4 +1,5 @@
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { useRouter } from "next/router";
 const menuItemStyles = makeStyles((theme: Theme) =>
     createStyles({
         menu_text: {
@@ -13,7 +14,7 @@ const menuItemStyles = makeStyles((theme: Theme) =>
             height: 64,
             display: "inherit",
             alignItems: "center",
-            borderTop: "1px solid black",
+            borderTop: "5px solid black",
             color: "#000",
             textAlign: "center",
             marginRight: 20,
@@ -25,8 +26,9 @@ interface MenuItemProps {
     pageURL?: string;
 }
 export default function MenuItemCustomed(props: MenuItemProps): JSX.Element {
+    const router = useRouter();
     const menuItem = menuItemStyles();
-    const this_page = window.location.pathname;
+    const this_page = router.pathname;
     if (props.pageURL == this_page) {
         return (
             <div className={menuItem.menu_text_selected}>
