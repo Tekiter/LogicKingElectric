b75f66d5dc67b52b9baa3da8b5b53cacaf9e78fb
Tekiter <tekiter8@gmail.com>
2021-07-26 23:44:09 +0900

    기능: 실제/더미 API 선택기능 추가

--- packages/server/src/app/index.ts
+++ packages/server/src/app/index.ts
@@ -2,20 +2,22 @@ import express from "express";
 import morgan from "morgan";
 import { createMemoryDataAccessFacade } from "../core/dataAccess/memory";
 import { SolarSimulationAPI } from "../core/openAPI/maruSolarSimulation";
+import { SolarSimulationAxiosCall } from "../core/openAPI/maruSolarSimulation/axiosCall";
 import { SolarSimulationDummyCall } from "../core/openAPI/maruSolarSimulation/dummyCall";
-import { OpenWeatherAPI, OpenWeatherAPIDummy } from "../core/openAPI/openWeather";
+import { OpenWeatherAPI, OpenWeatherAPIAxios, OpenWeatherAPIDummy } from "../core/openAPI/openWeather";
 import { createServices } from "../services";
 import APIv1 from "./api/v1";
 import { initialize } from "./initialize";
 
 export async function createApp(): Promise<express.Express> {
+    await loadDotEnvIfNotProduction();
+
     const app = express();
 
     app.use(devLogger());
 
     const dataAccess = createMemoryDataAccessFacade();
-    const solarSimulationAPI = createSolarSimulationAPI();
-    const openWeatherAPI = createOpenWeatherAPI();
+    const [solarSimulationAPI, openWeatherAPI] = createOpenAPI();
     const services = createServices(dataAccess, solarSimulationAPI, openWeatherAPI);
 
     await initialize(services);
@@ -31,12 +33,29 @@ function devLogger() {
     return morgan("dev");
 }
 
-function createSolarSimulationAPI(): SolarSimulationAPI {
-    return new SolarSimulationDummyCall();
-    // return new SolarSimulationAxiosCall();
+function createOpenAPI(): [SolarSimulationAPI, OpenWeatherAPI] {
+    if (process.env.USE_REAL_API === "TRUE") {
+        console.log("[info]: Now using real open API calls.");
+        return [new SolarSimulationAxiosCall(), createOpenWeatherAPI()];
+    } else {
+        console.log("[info]: Now using dummy API calls.");
+        return [new SolarSimulationDummyCall(), new OpenWeatherAPIDummy()];
+    }
 }
 
 function createOpenWeatherAPI(): OpenWeatherAPI {
-    return new OpenWeatherAPIDummy();
-    // return new OpenWeatherAPIAxios("API_KEY");
+    const key = process.env.OPEN_WEATHER_API_KEY;
+    if (key === undefined || key === "") {
+        console.info("[warn]: OpenWeather API Key is not set in environment value. Now using dummy API.");
+        return new OpenWeatherAPIDummy();
+    }
+
+    return new OpenWeatherAPIAxios(key);
+}
+
+async function loadDotEnvIfNotProduction() {
+    if (process.env.NODE_ENV !== "PRODUCTION") {
+        const dotenv = await import("dotenv");
+        dotenv.config();
+    }
 }
