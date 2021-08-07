6b1e936aad2e2cccc4d59babb9448541402ae9cf
goseungduk <goseungduk@gmail.com>
2021-08-07 16:39:29 +0900

    210807_tensorflow.js_테스팅코드

---
+++ packages/server/tests/test.ts
@@ -0,0 +1,13 @@
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-node";
+
+async function load(){
+    const model = await tf.loadLayersModel("file://./tests/model.json");
+    const dummy=tf.tensor3d([[[-0.8103514 ],[-0.61888821],[ 0.87503328],[-0.35512678],[0.11644669],[ 2.06014318],[-0.1549442 ],[-0.44740695],[ 1.13588825],[ 0.08193245],[ 0.126256  ],[ 0.19419455],[ 0.19419455],[ 0.33334142],[ 0.26794602],[ 0.01908021],[-1.0261562 ],[ 0.67085432],[ 1.17839526],[ 2.12844504]]])
+    const res= model.predict(dummy);
+    console.log((res as tf.Tensor).dataSync());
+    return model;
+}
+
+console.log('start');
+const model = load();
