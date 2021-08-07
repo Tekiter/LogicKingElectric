f0c1b43536cc19cde962c6c2854fddd21799058d
goseungduk <goseungduk@gmail.com>
2021-08-07 16:55:37 +0900

    210807_tensorflow.js_테스팅완료

--- packages/server/tests/test.ts
+++ packages/server/tests/test.ts
@@ -1,13 +1,38 @@
 import * as tf from "@tensorflow/tfjs";
 import "@tensorflow/tfjs-node";
 
-async function load(){
+async function load(sample: tf.Tensor3D) {
     const model = await tf.loadLayersModel("file://./tests/model.json");
-    const dummy=tf.tensor3d([[[-0.8103514 ],[-0.61888821],[ 0.87503328],[-0.35512678],[0.11644669],[ 2.06014318],[-0.1549442 ],[-0.44740695],[ 1.13588825],[ 0.08193245],[ 0.126256  ],[ 0.19419455],[ 0.19419455],[ 0.33334142],[ 0.26794602],[ 0.01908021],[-1.0261562 ],[ 0.67085432],[ 1.17839526],[ 2.12844504]]])
-    const res= model.predict(dummy);
-    console.log((res as tf.Tensor).dataSync());
-    return model;
+    const res = model.predict(sample);
+    //console.log((res as tf.Tensor).dataSync()[0]);
+    const model_prediction = (res as tf.Tensor).dataSync()[0];
+    return model_prediction;
 }
 
-console.log('start');
-const model = load();
+const dummy = tf.tensor3d([
+    [
+        [-0.8103514],
+        [-0.61888821],
+        [0.87503328],
+        [-0.35512678],
+        [0.11644669],
+        [2.06014318],
+        [-0.1549442],
+        [-0.44740695],
+        [1.13588825],
+        [0.08193245],
+        [0.126256],
+        [0.19419455],
+        [0.19419455],
+        [0.33334142],
+        [0.26794602],
+        [0.01908021],
+        [-1.0261562],
+        [0.67085432],
+        [1.17839526],
+        [2.12844504],
+    ],
+]);
+// kWh 단위의 20일치 발전량
+const res = load(dummy);
+res.then((val)=>console.log(val));
